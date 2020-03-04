9494a331875e87e285991d86ea14fab560ad325f
jacobjdong <jacobjdong@gmail.com>
2020-03-03 18:00:20 -0600

    Speech to text in frontend

---
+++ texttospeech/lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:speech_recognition/speech_recognition.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: VoiceHome(),
+    );
+  }
+}
+
+class VoiceHome extends StatefulWidget {
+  @override
+  _VoiceHomeState createState() => _VoiceHomeState();
+}
+
+class _VoiceHomeState extends State<VoiceHome> {
+  SpeechRecognition _speechRecognition;
+  bool _isAvailable = false;
+  bool _isListening = false;
+
+  String resultText = "";
+
+  @override 
+  void initState() {
+    super.initState();
+    initSpeechRecognizer();
+  }
+
+  void initSpeechRecognizer() {
+    _speechRecognition = SpeechRecognition();
+    _speechRecognition.setAvailabilityHandler(
+      (bool result) => setState(() => _isAvailable = result)
+    );
+
+    _speechRecognition.setRecognitionStartedHandler(
+      () => setState(() => _isListening = true),
+    );
+
+    _speechRecognition.setRecognitionResultHandler(
+      (String speech) => setState(() => resultText = speech),
+    );
+
+    _speechRecognition.setRecognitionCompleteHandler(
+      () => setState(() => _isListening = false),
+    );
+
+    _speechRecognition.activate().then(
+      (result) => setState(() => _isAvailable = result),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FloatingActionButton(
+                  child: Icon(Icons.mic),
+                  onPressed: () {
+                    if (_isAvailable && !_isListening)
+                      _speechRecognition
+                        .listen(locale: "en_US")    // THIS IS WHERE WE CAN DO KOREAN!!! Locale: korean etc
+                        .then((result) => print('$result'));
+                  },
+                  backgroundColor: Colors.lightBlue,
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    if (_isListening)
+                      _speechRecognition.stop().then(
+                        (result) => setState(() => _isListening = result),
+                      );
+                  },
+                ),
+              ],
+            ),
+
+            Container(                      // MOST OF THIS CAN BE DELETED? Just utilize resultText
+              width: MediaQuery.of(context).size.width * 0.8,
+              decoration: BoxDecoration(    // THIS CAN BE DELETED LATER
+                color: Colors.cyanAccent[100],
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              padding: EdgeInsets.symmetric(
+                vertical: 8.0,
+                horizontal: 12.0,
+              ),
+              child: Text(
+                resultText,
+                style: TextStyle(fontSize: 24.0),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
